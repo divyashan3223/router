817f60ebc1a84fa38a4f66972780293e23f468fc
frenzo technology <divyashan3223@gmail.com>
2024-03-25 12:04:14 +0530

    router intro

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>there is no doc for localhost:3000</p>
+    </div>
+  );
+};
+
+export default Error;
